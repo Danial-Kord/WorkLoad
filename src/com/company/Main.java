c2886facfc960c6bc868bf509e6e4e49771932ac
Danial-Kord <33172110+Danial-Kord@users.noreply.github.com>
2019-05-22 00:59:47 +0430

    i guess that's it! the algorithm faze 1 complete i guess now faze 2 is left:/(really!) faze 2 : we should shift all works back as it is possible!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,7 +13,7 @@ public class Main {
         int n = scanner.nextInt(); //job
         int m = scanner.nextInt();//station
         ArrayList<WorkStation>workStations = new ArrayList<WorkStation>();
-
+        long finalSum = 0;
         for (int i=0;i<n;i++) {
             workStations.add(new WorkStation(i));
         }
@@ -26,6 +26,7 @@ public class Main {
                 int time = scanner.nextInt();
                 workStations.get(j).setSum(workStations.get(j).getSum()+time);
                 jobs.get(jobs.size()-1).addTime(time);
+                finalSum += time;
             }
         }
 
@@ -36,16 +37,30 @@ public class Main {
                        jobs.set(i,jobs.get(j));
                        jobs.set(j,temp);
                    }
-                if(workStations.get(i).getSum()> workStations.get(j).getSum()){//sorting workStation array
-                    WorkStation temp = workStations.get(i);
-                    workStations.set(i,workStations.get(j));
-                    workStations.set(j,temp);
-                }
             }
         }
         for (int i =0;i<n;i++)
             System.out.println(jobs.get(i).getAvailableTime());
 
-
+        for (int i=n-1;i>=0;i--){
+            for (int j=0;j<workStations.size();j++){
+                workStations.get(j).setSum(workStations.get(j).getSum()-jobs.get(i).getWorkStationTimeSpend().get(workStations.get(j).getIndex()));
+                finalSum-=jobs.get(i).getWorkStationTimeSpend().get(i);
+            }
+            for (int j=0;j<m;j++){
+                for (int k=j;k<m;k++){
+                    if(workStations.get(j).getSum()> workStations.get(k).getSum()){//sorting workStation array
+                        WorkStation temp = workStations.get(j);
+                        workStations.set(j,workStations.get(k));
+                        workStations.set(k,temp);
+                    }
+                }
+            }
+            int uper = 0;
+            for (int j=0;j<m;j++){
+                jobs.get(i).getPos().set(workStations.get(j).getIndex(), (int) (finalSum+jobs.get(i).getAvailableTime())+uper);
+                uper+=jobs.get(i).getAvailableTime();
+            }
+        }
     }
 }
