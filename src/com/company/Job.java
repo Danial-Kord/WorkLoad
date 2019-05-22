ec8cc4817486a6682c3bcdac423f1f05a3783416
Danial-Kord <33172110+Danial-Kord@users.noreply.github.com>
2019-05-22 13:28:03 +0430

    faze 2 finished and extremely completed ! now we can find one of the near efficient answers.

--- src/com/company/Job.java
+++ src/com/company/Job.java
@@ -5,16 +5,27 @@ import java.util.ArrayList;
 public class Job {
     private int availableTime;
     private ArrayList<Integer> pos;
+    private ArrayList<Integer> priorites;
     private ArrayList<Integer> workStationTimeSpend; // index refers to witch station and value referes to what time it tackes to do that job in that station
 
     public Job(int availableTime) {
         this.availableTime = availableTime;
-
+        pos = new ArrayList<Integer>();
+        priorites = new ArrayList<Integer>();
         workStationTimeSpend = new ArrayList<Integer>();
     }
     public void addPos(Integer position){
+
         pos.add(position);
     }
+    public void addPriority(Integer in){
+        priorites.add(in);
+    }
+
+    public ArrayList<Integer> getPriorites() {
+        return priorites;
+    }
+
     public void addTime(Integer spendTime){
         workStationTimeSpend.add(spendTime);
     }
@@ -29,5 +40,18 @@ public class Job {
 
     public int getAvailableTime() {
         return availableTime;
+    }
+    public String info(){
+        String out = "available time : ";
+        out+=""+availableTime+"\n";
+        out+="pos : ";
+        for (int i=0;i<pos.size();i++)
+            out+=""+pos.get(i) + " ";
+        out+="\n time spend ";
+        for (int i=0;i<workStationTimeSpend.size();i++)
+            out+=""+workStationTimeSpend.get(i) + " ";
+        out+= "\n\n\n";
+        return out;
+
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,60 +7,88 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         ArrayList<Job> jobs = new ArrayList<Job>();
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt(); //job
         int m = scanner.nextInt();//station
-        ArrayList<WorkStation>workStations = new ArrayList<WorkStation>();
+        ArrayList<WorkStation> workStations = new ArrayList<WorkStation>();
         long finalSum = 0;
-        for (int i=0;i<n;i++) {
+        for (int i = 0; i < m; i++) {
             workStations.add(new WorkStation(i));
         }
 
-        for (int i =0;i<n;i++){//inputs and adding sum into workstation array
+        for (int i = 0; i < n; i++) {//inputs and adding sum into workstation array
             int availableTime = scanner.nextInt();
             jobs.add(new Job(availableTime));
 
-            for (int j=0;j<m;j++){
+            for (int j = 0; j < m; j++) {
                 int time = scanner.nextInt();
-                workStations.get(j).setSum(workStations.get(j).getSum()+time);
-                jobs.get(jobs.size()-1).addTime(time);
+                workStations.get(j).setSum(workStations.get(j).getSum() + time);
+                jobs.get(jobs.size() - 1).addTime(time);
+                jobs.get(jobs.size()-1).getPos().add(-1);
+                jobs.get(jobs.size()-1).addPriority(-1);
                 finalSum += time;
             }
         }
 
-        for (int i =0;i<n;i++){
-            for (int j=i;j<n;j++){
-                   if(jobs.get(i).getAvailableTime()> jobs.get(j).getAvailableTime()){//sorting jobs array by its available time field
-                       Job temp = jobs.get(i);
-                       jobs.set(i,jobs.get(j));
-                       jobs.set(j,temp);
-                   }
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if (jobs.get(i).getAvailableTime() > jobs.get(j).getAvailableTime()) {//sorting jobs array by its available time field
+                    Job temp = jobs.get(i);
+                    jobs.set(i, jobs.get(j));
+                    jobs.set(j, temp);
+                }
             }
         }
-        for (int i =0;i<n;i++)
+        for (int i = 0; i < n; i++)
             System.out.println(jobs.get(i).getAvailableTime());
 
-        for (int i=n-1;i>=0;i--){
-            for (int j=0;j<workStations.size();j++){
-                workStations.get(j).setSum(workStations.get(j).getSum()-jobs.get(i).getWorkStationTimeSpend().get(workStations.get(j).getIndex()));
-                finalSum-=jobs.get(i).getWorkStationTimeSpend().get(i);
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 0; j < workStations.size(); j++) {
+                workStations.get(j).setSum(workStations.get(j).getSum() - jobs.get(i).getWorkStationTimeSpend().get(workStations.get(j).getIndex()));
+                finalSum -= jobs.get(i).getWorkStationTimeSpend().get(j);
             }
-            for (int j=0;j<m;j++){
-                for (int k=j;k<m;k++){
-                    if(workStations.get(j).getSum()> workStations.get(k).getSum()){//sorting workStation array
+            for (int j = 0; j < m; j++) {
+                for (int k = j; k < m; k++) {
+                    if (workStations.get(j).getSum() > workStations.get(k).getSum()) {//sorting workStation array
                         WorkStation temp = workStations.get(j);
-                        workStations.set(j,workStations.get(k));
-                        workStations.set(k,temp);
+                        workStations.set(j, workStations.get(k));
+                        workStations.set(k, temp);
                     }
                 }
             }
             int uper = 0;
-            for (int j=0;j<m;j++){
-                jobs.get(i).getPos().set(workStations.get(j).getIndex(), (int) (finalSum+jobs.get(i).getAvailableTime())+uper);
-                uper+=jobs.get(i).getAvailableTime();
+            for (int j = 0; j < m; j++) {
+                jobs.get(i).getPos().set(workStations.get(j).getIndex(), (int) (finalSum + jobs.get(i).getAvailableTime()) + uper);
+                uper += jobs.get(i).getWorkStationTimeSpend().get(workStations.get(j).getIndex());
+                jobs.get(i).getPriorites().set(workStations.get(j).getIndex(),j);
             }
         }
+        System.out.println("fase 1 :\n");
+        for (int i = 0; i < n; i++) {
+            System.out.println(jobs.get(i).info());
+        }
+
+        for (int i=1;i<n;i++){
+            Job cur = jobs.get(i);
+            Job pre = jobs.get(i-1);
+
+            int index = cur.getPriorites().indexOf(0);
+            cur.getPos().set(index,Math.max(cur.getAvailableTime(),pre.getPos().get(index)+pre.getWorkStationTimeSpend().get(index)));
+            for (int j=1;j<m;j++){
+                index = cur.getPriorites().indexOf(j);
+                cur.getPos().set(index,Math.max(cur.getAvailableTime(),Math.max(pre.getPos().get(index)+pre.getWorkStationTimeSpend().get(index),
+                        jobs.get(j).getPos().get(index-1)+jobs.get(j).getWorkStationTimeSpend().get(index-1))));
+            }
+        }
+
+
+        System.out.println("fase 2 :\n");
+        for (int i = 0; i < n; i++) {
+            System.out.println(jobs.get(i).info());
+        }
+
+
     }
 }
