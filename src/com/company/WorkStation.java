b762caa255eae8caf8337bcdc570d87552232a04
kash <amkkashani@gmail.com>
2019-05-22 18:22:50 +0430

    "firs testing for intlijj"

--- src/com/company/WorkStation.java
+++ src/com/company/WorkStation.java
@@ -1,13 +1,15 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class WorkStation {
 private int sum;
 private int index;
+private ArrayList<ArrayList<Integer>> timeLine =  new ArrayList<>();
 
     public WorkStation(int index) {
         this.index = index;
     }
-
     public int getSum() {
         return sum;
     }
